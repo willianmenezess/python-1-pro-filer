5cf1d85afc011560851138b3cb32c0b329f10570
willianmenezess <willmenezes@outlook.com.br>
2023-10-23 23:39:15 -0300

    feat: resolve requisito 3

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -54,7 +54,30 @@ import pytest
                 "First 5 files: []\n"
                 "First 5 directories: ['src', 'src/utils']\n"
             ),
-        )
+        ),
+        (
+            {
+                "all_files": [
+                    "src/__init__.py",
+                    "src/app.py",
+                    "src/utils/__init__.py",
+                    "src/utils/aux.py",
+                    "src/utils/aux2.py",
+                    "src/utils/aux3.py",
+                ],
+                "all_dirs": [
+                    "src",
+                    "src/utils"
+                ],
+            },
+            (
+                "Found 6 files and 2 directories\n"
+                "First 5 files: ['src/__init__.py', 'src/app.py', "
+                "'src/utils/__init__.py', 'src/utils/aux.py', "
+                "'src/utils/aux2.py']\n"
+                "First 5 directories: ['src', 'src/utils']\n"
+            ),
+        ),
     ],
 )
 def test_show_preview_sucess(capsys, mock_context, expected):
